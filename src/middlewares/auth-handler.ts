d2f157952ee7546e32d372fe7a0a62d6a1af4723
changhwan hwang <hch950627@naver.com>
2022-05-10 15:46:33 +0900

    chore: setting

--- src/middlewares/auth-handler.ts
+++ src/middlewares/auth-handler.ts
@@ -1,31 +1,31 @@
-import { Context } from '@ecubelabs/seed';
 import { unauthorized } from '@hapi/boom';
 import { Middleware } from 'koa';
-import { decodeUserToken } from '../libs/jwt';
-import { UserRepository } from '../services/user/infrastructure/repository';
+import type Container from 'typedi';
+import { decodeUserToken } from '../libs';
+// import { UserRepository } from '../services/user/infrastructure/repository';
 
 export const authHandlerMiddleware: Middleware = async (ctx, next) => {
-    const { context } = ctx.state as { context: Context };
-    const bearerToken = ctx.get('Authorization');
-    if (!bearerToken) {
-        throw unauthorized(`There is no token`);
-    }
+  const { context } = ctx.state as { context: Container };
+  const bearerToken = ctx.get('Authorization');
+  if (!bearerToken) {
+    throw unauthorized(`There is no token`);
+  }
 
-    const [tokenType, token] = bearerToken.split(' ');
-    if (tokenType !== 'Bearer') {
-        throw unauthorized(`Token type is not Bearer`);
-    }
+  const [tokenType, token] = bearerToken.split(' ');
+  if (tokenType !== 'Bearer') {
+    throw unauthorized(`Token type is not Bearer`);
+  }
 
-    const decoded = decodeUserToken(token);
-    if (typeof decoded === 'string' || decoded === null) {
-        throw unauthorized(`Invalid token`);
-    }
+  const decoded = decodeUserToken(token);
+  if (typeof decoded === 'string' || decoded === null) {
+    throw unauthorized(`Invalid token`);
+  }
 
-    const { id } = decoded;
+  const { id } = decoded;
 
-    const userRepository = context.get(UserRepository);
-    const user = await userRepository.findOneOrFail(id);
+  //   const userRepository = context.get(UserRepository);
+  //   const user = await userRepository.findOneOrFail(id);
 
-    ctx.state.user = user;
-    await next();
+  //   ctx.state.user = user;
+  await next();
 };
